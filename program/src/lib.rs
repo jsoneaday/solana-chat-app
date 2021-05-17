5cd6eaa4004e041d2045207f1b2b7abf31dc64e0
David Choi <admin@dzhaven.com>
2021-05-17 08:46:27 -0400

    update program and add test

--- program/src/lib.rs
+++ program/src/lib.rs
@@ -11,9 +11,26 @@ use solana_program::{
 
 #[derive(BorshSerialize, BorshDeserialize, Debug)]
 pub struct ChatMessage {
-    pub txt: String,
-    pub created_date: f64,
-    pub send_to: String
+    pub archive_id: String
+}
+
+#[derive(BorshSerialize, BorshDeserialize, Debug)]
+pub struct ChatMessageContainer {
+    pub archive_ids: Vec<ChatMessage>
+}
+
+// example arweave tx (length 43)
+// 1seRanklLU_1VTGkEk7P0xAwMJfA7owA1JHW5KyZKlY
+// ReUohI9tEmXQ6EN9H9IkRjY9bSdgql_OdLUCOeMEte0
+pub fn get_init_chat_message() -> ChatMessage {
+    ChatMessage{ archive_id: String::from("0000000000000000000000000000000000000000000") }
+}
+pub fn get_init_chat_messages() -> Vec<ChatMessage> {
+    let mut messages = Vec::new();
+    for _ in 0..10 {
+        messages.push(get_init_chat_message());
+    }
+    return messages;
 }
 
 entrypoint!(process_instruction);
@@ -33,24 +50,35 @@ pub fn process_instruction(
 
     sol_log_compute_units();
 
-    log_instruction_data(instruction_data)?;
+    let instruction_data_message = ChatMessage::try_from_slice(instruction_data).map_err(|err| {
+        msg!("Attempt to deserialize instruction data has failed. {:?}", err);
+        ProgramError::InvalidInstructionData
+    })?;
+    msg!("Instruction_data message object {:?}", instruction_data_message);
 
+    let mut existing_data_messages = ChatMessageContainer::try_from_slice(&account.data.borrow_mut()).map_err(|err| {
+        msg!("Failed to decode account data. {:?}", err);
+        ProgramError::InvalidInstructionData
+    })?;
+    let index = existing_data_messages.archive_ids.iter().position(|p| p.archive_id == String::from("0000000000000000000000000000000000000000000")).unwrap(); // find first dummy data entry
+    msg!("Existing archive_id {}", existing_data_messages.archive_ids[index].archive_id);
+    existing_data_messages.archive_ids[index] = instruction_data_message; // set dummy data to new entry
+    msg!("Set existing_data_message");
+    let updated_data = existing_data_messages.try_to_vec().expect("Failed to encode data."); // set messages object back to vector data
+
+    // data algorithm for storing data into account and then archiving innto Arweave
+    // 1. Each ChatMessage object will be prepopulated for txt field having 140 characters.
+    // Each ChatMessageContainer will be prepopulated with 10 ChatMessage objects with dummy data.
+    // 2. Only up to 10 vector elements will be allowed in a ChatMessageContainer
+    // 3. Client side will test data before each add for element count
+    // 4. If count is 10 this data will be archived to arweave and then overwritten to 0's.
     let data = &mut &mut account.data.borrow_mut();
-    data[..instruction_data.len()].copy_from_slice(&instruction_data);    
+    msg!("Attempting save data.");
+    data[..updated_data.len()].copy_from_slice(&updated_data);    
     msg!("ChatMessage has been saved to account data.");
     sol_log_compute_units();
 
     msg!("End program.");
-    Ok(())
-}
-
-fn log_instruction_data(instruction_data: &[u8]) -> ProgramResult {
-    let message = ChatMessage::try_from_slice(instruction_data).map_err(|err| {
-        msg!("Attempt to deserialize data has failed! {:?}", err);
-        ProgramError::InvalidInstructionData
-    })?;
-    msg!("ChatMessage given to program is {:?}", message);
-    sol_log_compute_units();
     Ok(())
 }
 
@@ -59,48 +87,55 @@ fn log_instruction_data(instruction_data: &[u8]) -> ProgramResult {
 mod test {
     use super::*;
     use solana_program::clock::Epoch;
-    use std::mem;
+    //use std::mem;
 
     #[test]
     fn test_sanity() {
         let program_id = Pubkey::default();
         let key = Pubkey::default();
         let mut lamports = 0;
-        let mut data = vec![0; mem::size_of::<u32>()];
+        let data = ChatMessageContainer{ archive_ids: get_init_chat_messages() }; // vec![0; get_init_chat_messages().len()];
+        let mut data_data = data.try_to_vec().unwrap();
         let owner = Pubkey::default();
         let account = AccountInfo::new(
             &key,
             false,
             true,
             &mut lamports,
-            &mut data,
+            &mut data_data,
             &owner,
             false,
             Epoch::default(),
         );
-        let instruction_data: Vec<u8> = Vec::new();
+        
+        let instruction_data_chat_message = ChatMessage{ archive_id: String::from("abcdefghijabcdefghijabcdefghijabcdefghijabc") };
+        let instruction_data = instruction_data_chat_message.try_to_vec().unwrap();
 
         let accounts = vec![account];
 
-        assert_eq!(
-            ChatMessage::try_from_slice(&accounts[0].data.borrow())
-                .unwrap()
-                .txt,
-            0
-        );
         process_instruction(&program_id, &accounts, &instruction_data).unwrap();
+        let result = &ChatMessageContainer::try_from_slice(&accounts[0].data.borrow())
+            .unwrap()
+            .archive_ids[0].archive_id;
+        println!("archive_id {}", result);
+        // I added first data and expect it to contain the given data
         assert_eq!(
-            ChatMessage::try_from_slice(&accounts[0].data.borrow())
-                .unwrap()
-                .txt,
-            1
+            String::from("abcdefghijabcdefghijabcdefghijabcdefghijabc").eq(result),
+            true
         );
-        process_instruction(&program_id, &accounts, &instruction_data).unwrap();
-        assert_eq!(
-            ChatMessage::try_from_slice(&accounts[0].data.borrow())
-                .unwrap()
-                .txt,
-            2
-        );
+        // process_instruction(&program_id, &accounts, &instruction_data).unwrap();
+        // assert_eq!(
+        //     ChatMessageContainer::try_from_slice(&accounts[0].data.borrow())
+        //         .unwrap()
+        //         .txt,
+        //     1
+        // );
+        // process_instruction(&program_id, &accounts, &instruction_data).unwrap();
+        // assert_eq!(
+        //     ChatMessageContainer::try_from_slice(&accounts[0].data.borrow())
+        //         .unwrap()
+        //         .txt,
+        //     2
+        // );
     }
 }
